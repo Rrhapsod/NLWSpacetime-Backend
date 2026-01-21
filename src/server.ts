d4fe503e2a29c2f0930df9571dcb8b14fc1e7b4a
Renan Rondon <renanrondon@live.com>
2026-01-21 14:29:45 -0300

    vercel deploy configs 3

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-
 import { memoriesRoutes } from "./routes/memories";
 
 const fastify = Fastify({
@@ -10,6 +9,10 @@ const fastify = Fastify({
 fastify.register(cors, { origin: true });
 fastify.register(memoriesRoutes);
 
-fastify.listen({ port: 3333 }).then(() => {
-  console.log("Servidor rodando em: http://localhost:3333");
-});
+export default fastify;
+
+if (process.env.NODE_ENV !== "production") {
+  fastify.listen({ port: 3333 }).then(() => {
+    console.log("Servidor rodando em: http://localhost:3333");
+  });
+}
